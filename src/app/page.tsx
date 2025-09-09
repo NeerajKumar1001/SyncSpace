2c521442db9b2e38f0d3e2c1a73ed06b62cfe8a9
Neeraj Kumar <144528779+NeerajKumar1001@users.noreply.github.com>
2025-09-09 09:40:33 +0530

    done with settingup web hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import {SignedIn,SignedOut,SignInButton,UserButton,} from "@clerk/nextjs";
 import Image from "next/image";
+// import { Button } from "@/components/ui";
 
 export default function Home() {
   return (
     <div className="m-10">
-      Home page
+      <SignInButton>
+        <button>
+          login
+        </button>
+      </SignInButton>
     </div>
   );  
 }
